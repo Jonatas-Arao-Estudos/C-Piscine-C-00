02b4d6132862bdd59be3497217bfeab8f286d751
Jonatas Arão de Carvalho Neves <jonatas.arao.de.carvalho.neves@hotmail.com>
2024-08-19 14:26:11 -0300

    ex05 changing variables

--- ex05/ft_print_comb.c
+++ ex05/ft_print_comb.c
@@ -6,7 +6,7 @@
 /*   By: jarao-de <jarao-de@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/16 18:01:56 by jarao-de          #+#    #+#             */
-/*   Updated: 2024/08/16 21:14:51 by jarao-de         ###   ########.fr       */
+/*   Updated: 2024/08/17 13:23:39 by jarao-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,14 @@ void	ft_print_comb(void)
 {
 	char	number[3];
 
-	number[0] = 48;
-	number[1] = 48;
-	number[2] = 48;
-	while (number[0] <= 57)
+	number[0] = '0';
+	number[1] = '0';
+	number[2] = '0';
+	while (number[0] <= '9')
 	{
-		while (number[1] <= 57)
+		while (number[1] <= '9')
 		{
-			while (number[2] <= 57)
+			while (number[2] <= '9')
 			{
 				if (number[0] < number[1] && number[1] < number[2])
 				{
@@ -32,10 +32,10 @@ void	ft_print_comb(void)
 				}
 				number[2] = number[2] + 1;
 			}
-			number[2] = 48;
+			number[2] = '0';
 			number[1] = number[1] + 1;
 		}
-		number[1] = 48;
+		number[1] = '0';
 		number[0] = number[0] + 1;
 	}
 	write(1, "\b\b", 2);
